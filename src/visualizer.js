8b57fef34467ba5a5abd7eeaf65e6af5c7ea0fb7
pamika thanyacharoen <opera.pay@gmail.com>
2019-10-05 11:59:37 +0700

    draw-line-on-map

--- src/visualizer.js
+++ src/visualizer.js
@@ -6,71 +6,67 @@ import * as maptalks from 'maptalks'
 import './visualizer.css'
 import * as d3 from 'd3-request';
 import url from './data/data_flight.csv';
-
-var a = [
-['100.758698', '13.656518', '472.44'],
-['100.764381', '13.654377', '495.3'],
-['100.769302', '13.653166', '518.16'],
-['100.777298', '13.651397', '571.5'],
-['100.786301', '13.64946', '617.22'],
-['100.794395', '13.647675', '693.42'],
-['100.799324', '13.646576', '739.14'],
-['100.807846', '13.644562', '800.1'],
-['100.818207', '13.642087', '883.92'],
-['100.827271', '13.640038', '960.12'],
-['100.844833', '13.636093', '1082.04'],
-['100.850845', '13.63472', '1127.76']]
-
-var dataAll = [{name:'PG203/BKP203', coords: [['100.759529', '13.692165', '0'],
-['100.759804', '13.69202', '0'],
-['100.759804', '13.69202', '0'],
-['100.760376', '13.69186', '0'],
-['100.760818', '13.691106', '0'],
-['100.757057', '13.676116', '144.78'],
-['100.755806', '13.670484', '259.08'],
-['100.754715', '13.664566', '365.76'],
-['100.75547', '13.660242', '441.96']]}]
-
-var dataset;
-d3.csv(url, function(error, data) {
-    dataset = data
-    return dataset
-});
-
-console.log(dataset)
-    // dataAll[0].name = data[0].name
-    // for(var i=0;i<21;i++){
-    //     dataAll[0].coords[i][0] = a[i][0]
-    //     dataAll[0].coords[i][1] = a[i][1]
-    //     dataAll[0].coords[i][2] = a[i][2]
-    //     // dataAll[0].coords[i].push(data[i].long)
-    //     // dataAll[0].coords[i].push(data[i].lat)
-    //     // dataAll[0].coords[i].push(data[i].attitude)
-    // }
-
-// var dataAll = [{name:'PG203/BKP203', coords: [['100.759529', '13.692165', '0'],
-// ['100.759804', '13.69202', '0'],
-// ['100.759804', '13.69202', '0'],
-// ['100.760376', '13.69186', '0'],
-// ['100.760818', '13.691106', '0'],
-// ['100.757057', '13.676116', '144.78'],
-// ['100.755806', '13.670484', '259.08'],
-// ['100.754715', '13.664566', '365.76'],
-// ['100.75547', '13.660242', '441.96'],
-// ['100.758698', '13.656518', '472.44'],
-// ['100.764381', '13.654377', '495.3'],
-// ['100.769302', '13.653166', '518.16'],
-// ['100.777298', '13.651397', '571.5'],
-// ['100.786301', '13.64946', '617.22'],
-// ['100.794395', '13.647675', '693.42'],
-// ['100.799324', '13.646576', '739.14'],
-// ['100.807846', '13.644562', '800.1'],
-// ['100.818207', '13.642087', '883.92'],
-// ['100.827271', '13.640038', '960.12'],
-// ['100.844833', '13.636093', '1082.04'],
-// ['100.850845', '13.63472', '1127.76']]}]
+import Papa from 'papaparse'
 
 class Flightpath extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dataAll : [{name:'', coords: [['', '', '']]}],
+            data: [],
+            arr: [{
+                name:'',
+                coords: [[]]
+            }]
+        };
+
+        this.getData = this.getData.bind(this);
+        this.test = this.test.bind(this)
+    }
+
+    componentWillMount() {
+        this.getCsvData();
+    }
+
+    fetchCsv() {
+        return fetch(url).then(function (response) {
+            let reader = response.body.getReader();
+            let decoder = new TextDecoder('utf-8');
+
+            return reader.read().then(function (result) {
+                return decoder.decode(result.value);
+            });
+        });
+    }
+
+    getData(result) {
+        this.setState({data: result.data});
+        this.test()
+        this.setState({dataAll: this.state.arr});
+    }
+
+    test(){
+        //console.log(this.state.data)
+        for(var i=1;i<101;i++){
+            this.state.arr[0].name = this.state.data[1][1]
+            this.state.arr[0].coords[i-1].push(this.state.data[i][3])
+            this.state.arr[0].coords[i-1].push(this.state.data[i][4])
+            this.state.arr[0].coords[i-1].push(this.state.data[i][5])
+            if(i < 100){
+                this.state.arr[0].coords.push([])
+            }
+        }
+        //console.log(this.state.arr)
+    }
+
+    async getCsvData() {
+        let csvData = await this.fetchCsv();
+
+        Papa.parse(csvData, {
+            complete: this.getData
+        });
+    }
     getOption = () => ({
         maptalks3D: {
             // center: [-0.4855, 51.4727],
@@ -98,7 +94,8 @@ class Flightpath extends Component {
                 }
             }
         },
-        series: [{
+        series: [
+            {
             type: 'lines3D',
             coordinateSystem: 'maptalks3D',
             effect: {
@@ -115,17 +112,38 @@ class Flightpath extends Component {
                 color: 'rgb(50, 60, 170)',
                 opacity: 0.5
             },
-            data: dataAll
-        }]
-      });
+            data: this.state.dataAll
+        }
+        // {
+        //     type: 'surface',
+        //     //coordinateSystem: 'maptalks3D',
+        //     wireframe: {
+        //         show: false
+        //     },
+        //     equation: {
+        //         x: {
+        //             step: 0.05
+        //         },
+        //         y: {
+        //             step: 0.05
+        //         },
+        //         z: function (x, y) {
+        //             if (Math.abs(x) < 0.1 && Math.abs(y) < 0.1) {
+        //                 return '-';
+        //             }
+        //             return Math.sin(x * Math.PI) * Math.sin(y * Math.PI);
+        //         }
+        //     }
+        // }],
+        ]
+    });
     
-      render() {
-          
+    render() {
         return (
             <div className='map' >
                 <ReactEcharts option={this.getOption()} style={{width:1500, height:700}} />
             </div>
         );
-      }
+    }
 }
 export default Flightpath;
