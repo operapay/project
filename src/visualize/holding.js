8dd3df0bf362c9bc1e9dbe92f380393064c053ba
pamika thanyacharoen <opera.pay@gmail.com>
2019-11-19 15:03:57 +0700

    select holding flight

--- src/visualize/holding.js
+++ src/visualize/holding.js
@@ -5,10 +5,14 @@ import 'mapbox-echarts'
 import * as maptalks from 'maptalks'
 import './holding.css'
 import * as d3 from 'd3-request';
-// import url from '../data/data_28.10.62.csv';
-import url from '../data/data_test.csv';
+import url from '../data/data_28.10.62.csv';
+// import url from '../data/data_test.csv';
 import Papa from 'papaparse'
 import echarts from 'echarts'
+import { Select } from 'antd';
+
+const { Option } = Select;
+
 
 var map = {
     center: [100.7395539,13.6983666], //mahamek
@@ -22,17 +26,6 @@ var map = {
     }),
 }
 
-var dataplane = [
-    // {name:'MP19R-RYAUN', coords: [[100.7432,13.70367,0],
-    // [100.7694,13.80349,609.6],
-    // [100.7858,13.86579,762],
-    // [100.8056,13.94083,1066.8]]},
-    {name:'LOUIS-BS902', coords: [[100.7518,13.6567,0],
-    [100.7369,13.59995,0],
-    [100.798,13.51101,0],
-    [100.8975,13.51687,1828.8]]}
-]
-
 
 var centrallocat = [[100.7415433,13.6383389,23]]
 
@@ -42,16 +35,13 @@ class Flightpath extends React.Component {
 
         this.state = {
             dataAll : [{name:'', coords: [['', '', '']]}],
-            data: [{name:'aaa', coords:[["100.759529", "13.692165", "0"],
-            ["100.759804", "13.69202", "0"],
-            ["100.759804", "13.69202", "0"],
-            ["100.760376", "13.69186", "0"],
-            ["100.760818", "13.691106", "0"],
-            ["100.757057", "13.676116", "144.78"]]}],
             arr: [{
                 name:'',
                 coords: [[]]
             }],
+            arr_select : [],
+            select : "",
+            dataHolding : [{name:'', coords: [['', '', '']]}],
         };
 
         this.getData = this.getData.bind(this);
@@ -75,24 +65,84 @@ class Flightpath extends React.Component {
 
     uniqueNameFlight(name,data,date){
         var count = 0
+        console.log(data.length)
         for(var i=1;i<data.length;i++){
             if (data[i][1] === '-'){
                 count += 1
-                console.log(i)
+                // console.log(i)
             }
         }
         return count
     }
 
+    distance(lat1, lon1, lat2, lon2, unit) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            var radlat1 = Math.PI * lat1/180;
+            var radlat2 = Math.PI * lat2/180;
+            var theta = lon1-lon2;
+            var radtheta = Math.PI * theta/180;
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            if (dist > 1) {
+                dist = 1;
+            }
+            dist = Math.acos(dist);
+            dist = dist * 180/Math.PI;
+            dist = dist * 60 * 1.1515;
+            if (unit==="K") { dist = dist * 1.609344 }
+            if (unit==="N") { dist = dist * 0.8684 }
+            // console.log('nmi') }
+            return dist;
+        }
+    }
+
+    handleChange(value,data) {
+        var data_select = []
+        //console.log(value,data)
+        // this.setState({select : value})
+        for(var i=0;i<data.length;i++){
+            if(data[i].name === value){
+                data_select.push(data[i])
+                console.log(data[i])
+            }
+        }
+        console.log(data_select)
+        this.setState({dataHolding : data_select})
+    }
+
+    selectdata(flighthold,data){
+        var data_holding = []
+        for(var i=0;i<data.length;i++){
+            for(var j=0;j<flighthold.length;j++){
+                if(data[i].name === flighthold[j]){
+                    data_holding.push(data[i])
+                    // console.log(data[i].name)
+                }
+            }
+        }
+        // console.log(this.state.data_holding)
+        this.setState({dataAll: data_holding});
+    }
+
     getData(result) {
+        console.log(result.data.length)
         var num = 1
         var name = result.data[1][1]
         var date = result.data[1][1]
         var count = this.uniqueNameFlight(name,result.data,date)
+        var dis = 100000
+        var arr = []
+        var check = false
+        var sum = 0
+        var dist
         console.log(count)
 
         for(var j=0;j<count;j++){
-            //console.log(j)
+            dis = 100000
+            check = false
+            sum = 0
             for(var i=num;i<=result.data.length;i++){
                 // console.log(num)
                 if(result.data[i][1] === '-'){
@@ -106,17 +156,40 @@ class Flightpath extends React.Component {
                 this.state.arr[j].coords[i-num].push(result.data[i][4])
                 this.state.arr[j].coords[i-num].push(result.data[i][5])
                 this.state.arr[j].coords[i-num].push(result.data[i][6])
+                // dist = this.distance(13.6567,100.7518,result.data[i][5],result.data[i][4],"K")
+                // console.log(result.data[i][1]," ", dist)
+                if(check == false){
+                    dist = this.distance(13.6567,100.7518,result.data[i][5],result.data[i][4],"N")
+                    if(dist > dis && (dist > 20 & dist < 50)){
+                        // console.log(result.data[i][1]," ", dist)
+                        sum = sum + 1
+                        if(sum > 10){
+                            // console.log(sum)
+                            check = true
+                            name = result.data[i][1]
+                        }
+                        
+                    }
+                    dis = this.distance(13.6567,100.7518,result.data[i][5],result.data[i][4],"N")
+                    // console.log(result.data[i][1]," ", this.distance(13.6567,100.7518,result.data[i][5],result.data[i][4],"N"))
+                }
+            }
+            if(check == true){
+                arr.push(name)
             }
             // console.log(j)
             if(j < count-1){
                 this.state.arr.push({name:'', coords: [[]]})
             }
         }
+        console.log(arr)
         //console.log(result.data)
         // this.setState({test: result.data});
         console.log(this.state.arr)
         //this.test()
-        this.setState({dataAll: this.state.arr});
+        this.setState({arr_select : arr})
+        this.selectdata(arr,this.state.arr)
+        // this.setState({dataAll: this.state.arr});
     }
 
 
@@ -131,30 +204,6 @@ class Flightpath extends React.Component {
     getOption = () => ({
         maptalks3D: map, 
         series: [
-            // {
-            //     type: 'lines3D',
-            //     coordinateSystem: 'maptalks3D',
-            //     polyline: true,
-            //     // silent: true,
-            //     lineStyle: {
-            //         width: 50,
-            //         color: 'red',
-            //         opacity: 0.3,
-            //     },
-            //     data: dataplane
-            // },
-            // {
-            //     type: 'lines3D',
-            //     coordinateSystem: 'maptalks3D',
-            //     polyline: true,
-            //     // silent: true,
-            //     lineStyle: {
-            //         width: 5,
-            //         color: 'red',
-            //         opacity: 0.3,
-            //     },
-            //     data: dataplane
-            // },
             {
                 type: 'bar3D',
                 coordinateSystem: 'maptalks3D',
@@ -185,7 +234,7 @@ class Flightpath extends React.Component {
                     color: 'rgb(50, 60, 170)',
                     opacity: 0.5
                 },
-                data: this.state.dataAll
+                data: this.state.dataHolding
             }
         ],
     });
@@ -194,7 +243,12 @@ class Flightpath extends React.Component {
         // this.state.map.remove()
         return (
             <React.Fragment>
-                <p>Offset Visualization</p>
+                <p>Holding Visualization</p>
+                <Select placeholder="Select Flight" style={{ width: 120 }} onChange={e => this.handleChange(e,this.state.dataAll)}>
+                    {this.state.arr_select.map(flight => (
+                        <Option key={flight}>{flight}</Option>
+                    ))}
+                </Select>
                 <ReactEcharts option={this.getOption()} style={{width:1500, height:700}} />
             </React.Fragment>
             // <p>test</p>
