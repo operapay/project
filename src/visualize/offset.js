a16006a96a3fe9da7aabff65edd088b062c6d9ba
pamika thanyacharoen <opera.pay@gmail.com>
2019-12-17 01:35:22 +0700

    select offset

--- src/visualize/offset.js
+++ src/visualize/offset.js
@@ -6,6 +6,7 @@ import * as maptalks from 'maptalks'
 import './offset.css'
 import { Select } from 'antd';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const { Option } = Select;
 
@@ -40,7 +41,7 @@ class FileReader extends React.Component {
         super(props);
         this.state = {
             // csvfile: undefined,
-            dataAll : [{name:'', coords: [['', '', '']]}],
+            dataAll : [{name:'', coords: [['', '', '']],date:'',time_1:'',time_2:''}],
             data: [{name:'aaa', coords:[["100.759529", "13.692165", "0"],
             ["100.759804", "13.69202", "0"],
             ["100.759804", "13.69202", "0"],
@@ -49,8 +50,18 @@ class FileReader extends React.Component {
             ["100.757057", "13.676116", "144.78"]]}],
             arr: [{
                 name:'',
-                coords: [[]]
+                coords: [[]],
+                date:'',
+                time_1:'',
+                time_2:''
             }],
+            distinct_date : [],
+            distinct_time : [],
+            select_date : false,
+            date_time : [],
+            date_name : [],
+            time_name : [],
+            flight : []
         };
         this.test = props.data
         this.check = props.check
@@ -90,6 +101,41 @@ class FileReader extends React.Component {
         return count
     }
 
+    Date_onhandleChange(value,data) {
+        var data_select = []
+        var data_time = []
+        this.setState({select_date:true})
+        // console.log(value,data)
+        // this.setState({select : value})
+        for(var i=0;i<data.length;i++){
+            // console.log(data[i].date, String(value))
+            if(data[i].date === String(value)){
+                data_select.push(data[i])
+                data_time.push(data[i].time_1.getHours())
+                // console.log(data[i])
+            }
+        }
+        var distinct = [...new Set(data_time)].sort()
+        // console.log(data_select)
+        this.setState({distinct_time : distinct,date_name:data_select})
+    }
+
+    Time_onhandleChange(value,data) {
+        var data_select = []
+        // this.setState({dataHolding : [], checkedList:[]})
+        // console.log(value,data)
+        // this.setState({select : value})
+        for(var i=0;i<data.length;i++){
+            // console.log(data[i].time_1.getHours())
+            if(data[i].time_1.getHours() === parseInt(value) || data[i].time_2.getHours() === parseInt(value)){
+                data_select.push(data[i])
+                console.log(data[i])
+            }
+        }
+        console.log(data_select)
+        this.setState({flight : data_select})
+    }
+
     getData(result) {
         this.state.arr = [{
             name:'',
@@ -99,13 +145,24 @@ class FileReader extends React.Component {
         var name = result[0].name
         var date = result[0].name
         var count = this.uniqueNameFlight(name,result,date)
+        var dataall_date = []
+        var data_check_time_date = []
         console.log(count)
 
         for(var j=0;j<count;j++){
             //console.log(j)
+            var mydate = moment(String(result[num].date), 'DD/MM/YYYY');
+            var date = moment(mydate).format("DD/MM/YYYY");
+            dataall_date.push(date)
             for(var i=num;i<=result.length;i++){
                 // console.log(num)
                 if(result[i].name === '-'){
+                    var time1 = new Date(moment(mydate).format("MM/DD/YYYY")+" " + result[num].time);
+                    var time2 = new Date(moment(mydate).format("MM/DD/YYYY")+" " + result[i-1].time);
+                    this.state.arr[j].date = date
+                    this.state.arr[j].time_1 = time1
+                    this.state.arr[j].time_2 = time2
+                    // data_check_time_date.push({name: result[num].name,date: date,time_1:time1,time_2:time2})
                     num = i+1
                     //name = result.data[i][1]
                     this.state.arr[j].coords.pop()
@@ -119,14 +176,21 @@ class FileReader extends React.Component {
             }
             // console.log(j)
             if(j < count-1){
-                this.state.arr.push({name:'', coords: [[]]})
+                this.state.arr.push({name:'', coords: [[]],date:'',time_1:'',time_2:''})
             }
         }
-        //console.log(result.data)
+        console.log(data_check_time_date)
         // this.setState({test: result.data});
-        console.log(this.state.arr)
+        // console.log(this.state.arr)
+        var distinct = [...new Set(dataall_date)]
+        distinct.sort(function(a, b){
+            var aa = a.split('/').reverse().join(),
+                bb = b.split('/').reverse().join();
+            return aa < bb ? -1 : (aa > bb ? 1 : 0);
+        });
+        // console.log('date: ',distinct)
         //this.test()
-        this.setState({dataAll: this.state.arr});
+        this.setState({dataAll: this.state.arr,distinct_date:distinct});
     }
 
     getOption = () => ({
@@ -186,7 +250,7 @@ class FileReader extends React.Component {
                     color: 'rgb(50, 60, 170)',
                     opacity: 0.5
                 },
-                data: this.state.dataAll
+                data: this.state.flight
             }
         ],
     });
@@ -196,6 +260,18 @@ class FileReader extends React.Component {
       return (
         <div className="App">
             <h1>Offset Visualization</h1>
+            <Select placeholder="Select Date" style={{ width: 200, fontSize: "1.2rem", paddingRight:"100 px" }} onChange={e => this.Date_onhandleChange(e,this.state.dataAll)}>
+                {this.state.distinct_date.map(flight => (
+                    <Option style={{ fontSize: "1rem" }} key={flight}>{flight}</Option>
+                ))}
+            </Select>
+            {this.state.select_date === true ? 
+                <Select placeholder="Select Time" style={{ width: 200, fontSize: "1.2rem", paddingRight:"100 px" }} onChange={e => this.Time_onhandleChange(e,this.state.date_name)}>
+                    {this.state.distinct_time.map(flight => (
+                        <Option style={{ fontSize: "1rem" }} key={flight}>{flight}.00 - {flight}.59</Option>
+                    ))}
+                </Select>: null
+            }
             <ReactEcharts option={this.getOption()} style={{width:1760, height:600}} />
         </div>
       );
