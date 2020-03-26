3d0cd49bebee0efed9b647cc657b22f356eb56e4
pamika thanyacharoen <opera.pay@gmail.com>
2020-03-26 22:59:42 +0700

    update page

---
+++ precache-manifest.d9c0b0b7a84aead04450bbb82d026dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90baef04d1c59b15248fdefda74ec11d",
+    "url": "https://operapay.github.io/project/index.html"
+  },
+  {
+    "revision": "83deb6de39ed497f73e3",
+    "url": "https://operapay.github.io/project/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "29c80297a289bdf94998",
+    "url": "https://operapay.github.io/project/static/css/main.93db2cb4.chunk.css"
+  },
+  {
+    "revision": "83deb6de39ed497f73e3",
+    "url": "https://operapay.github.io/project/static/js/2.09f74a20.chunk.js"
+  },
+  {
+    "revision": "29c80297a289bdf94998",
+    "url": "https://operapay.github.io/project/static/js/main.2c24481f.chunk.js"
+  },
+  {
+    "revision": "e585c9545aab2c931c08",
+    "url": "https://operapay.github.io/project/static/js/runtime-main.11fdfe1b.js"
+  }
+]);
